6e6ded125daacfa4d5acff03384eb550c74e8997
Usama Malik <usamamalik2033@gmail.com>
2025-07-25 15:53:41 +0500

    feat(refresh-token):implemented refresh & access token

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -2,8 +2,7 @@ import { registerAs } from "@nestjs/config";
 
 export default registerAs('jwt', () => ({
     secret: process.env.JWT_SECRET,
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    expiresIn: process.env.JWT_EXPIRES_IN || '15m',
     refreshSecret: process.env.JWT_SECRET,
-    refreshExpiresIn: process.env.JWT_EXPIRES_IN,
-})
-)
+    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d',
+}));
