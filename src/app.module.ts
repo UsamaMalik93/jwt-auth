833628c8100a7b482cd97668e6d3e96be749a09a
Usama Malik <usamamalik2033@gmail.com>
2025-07-25 15:56:27 +0500

    feat(rate-limiting ):IMmplemented rate limit for access control on endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,17 @@ import jwtConfig from './config/jwt.config';
 import { UsersModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal:true,
       load:[jwtConfig]
+    }),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
     }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     UsersModule,
